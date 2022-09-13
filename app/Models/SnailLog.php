2d5c89a6e0967f46247c5e23c8ba5a888ecc7b4d
Eugenio <esarrit@gmail.com>
2022-09-13 17:33:35 -0400

    add comments

--- app/Models/SnailLog.php
+++ app/Models/SnailLog.php
@@ -5,6 +5,11 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
+/**
+ * Represents the data model of the snail log. This maps directly to the database table
+ * called snail_logs via Laravel's Eloquent ORM (object relational mapper).
+ * 
+ */
 class SnailLog extends Model
 {
     public $timestamps = false;
